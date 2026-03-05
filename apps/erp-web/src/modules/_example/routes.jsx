951b3c505fa2871d1d5d7058bee2c6257f17df19
Abdul Moiz <abdulmoiz.trusoft@gmail.com>
2026-03-05 22:44:01 +0500

    push

--- apps/erp-web/src/modules/_example/routes.jsx
+++ apps/erp-web/src/modules/_example/routes.jsx
@@ -6,4 +6,8 @@ export const jobRequisitionsRoutes = [
     path: '/recruitment/job-requisitions',
     element: <ExampleScreen />,
   },
+   {
+    path: '/recruitment/job-requisitions',
+    element: <ExampleScreen />,
+  },
 ];
